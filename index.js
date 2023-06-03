1aebdc1b488d3112a90a7677724ca2698227c69f
Aniebietabasi <aniebietabasiobo@gmail.com>
2023-06-03 10:54:59 +0100

    commit

--- index.js
+++ index.js
@@ -1,56 +1,3 @@
-// const express = require("express");
-// require("dotenv").config();
-// require("./db");
-// const morgan = require("morgan");
-// const port = process.env.PORT || 3300;
-// const userRoutes = require('./routes/userRoutes');
-// const messageRoutes = require('./routes/messageRoutes');
-// const socket = require("socket.io");
-// const cors = require("cors");
-
-
-// const app = express();
-// app.use(cors());
-// const apiSeedUrl = '/api/v1';
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(morgan('dev'));
-// app.use(express.urlencoded({ extended: true }));
-
-
-// app.use(`${apiSeedUrl}/user`, userRoutes);
-// app.use(`${apiSeedUrl}/message`, messageRoutes);
-
-
-// const server = app.listen(port, () => {
-//     console.log(`Server listening on port ${port}`);
-//   });   
-
-
-//   const io = socket(server, {
-//     cors: {
-//       origin: "*",
-//       credentials: true,
-//       methods: ["GET", "POST"],
-//     },
-//   })
-
-
-//   global.onlineUser = new Map();
-
-//   io.on("connection", (socket) => {
-//     global.chatSocket = socket;
-//     socket.on("add-user",(userId)=>{
-//       onlineUser.set(userId,socket.id);
-//     })
-
-//     socket.on("send-msg", (data) => {
-//       const sendUserSocket = onlineUsers.get(data.to);
-//       if(sendUserSocket){
-//         io.to(sendUserSocket).emit("msg-recieve",data.msg);
-//       }
-//     })
-//   })
 
 const express = require("express");
 require("dotenv").config();
@@ -59,8 +6,9 @@ const morgan = require("morgan");
 const port = process.env.PORT || 3300;
 const userRoutes = require('./routes/userRoutes');
 const messageRoutes = require('./routes/messageRoutes');
-const socket = require("socket.io");
+const {Server} = require("socket.io");
 const cors = require("cors");
+const http = require("http");
 
 const app = express();
 app.use(cors());
@@ -69,15 +17,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
+const server = http.createServer(app);
 
 app.use(`${apiSeedUrl}/user`, userRoutes);
 app.use(`${apiSeedUrl}/message`, messageRoutes);
 
-const server = app.listen(port, () => {
+ app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });   
 
-const io = socket(server, {
+const io = new Server(server, {
   cors: {
     origin: "*",
     credentials: true,
@@ -88,6 +37,7 @@ const io = socket(server, {
 global.onlineUsers = new Map();
 
 io.on("connection", (socket) => {
+  console.log("Socket connected: ", socket.id);
   socket.on("add-user", (userId) => {
     global.onlineUsers.set(userId, socket.id);
   });
@@ -99,3 +49,5 @@ io.on("connection", (socket) => {
     }
   });
 });
+
+
